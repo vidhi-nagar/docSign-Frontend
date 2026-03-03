0e62236f9e5682ba51c9fcb6d879f014c9b922a6
Vidhi Nagar <vidhinagr6644@gmail.com>
2026-03-03 21:16:59 +0530

    backend url updated

--- src/context/axios.jsx
+++ src/context/axios.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    "https://pdf-sign-app-backend.vercel.app" || import.meta.env.VITE_API_URL,
+  baseURL: "https://pdf-sign-app-backend.vercel.app",
   withCredentials: true,
 });
